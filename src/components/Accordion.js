aee412f6a349ae59b841bb88d1266f9f979b0a0f
Luciano Hayer <lucianohayer@gmail.com>
2021-08-17 21:32:06 +0200

    Accordion interaction

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  
   const onTitleClick = (index) => {
-    console.log('Title clicked', index);
+    setActiveIndex(index);
   }
 
   const renderedItems = items.map((item, idx) => {
+    const active = idx === activeIndex ? 'active': '';
+
     return (
       <React.Fragment key={item.title}>
         <div
-          className="title active"
+          className={`title ${active}`}
           onClick={() => onTitleClick(idx)}
         >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{ item.content }</p>
         </div>
       </React.Fragment>
